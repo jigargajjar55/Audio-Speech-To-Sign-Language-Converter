c81a268fda4d7380ac22ff88d34c51b2028249c4
Jigar Gajjar <jigargajjar.55jg@gmail.com>
2023-06-14 23:39:17 +0530

    added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='audio-speech-to-sign-language-converter',
+    version='0.1.0',
+    description='Python project',
+    author='Jigar Gajjar',
+    author_email='jigargajjar.55jg@gmail.com',
+    url='https://github.com/jigargajjar55/Audio-Speech-To-Sign-Language-Converter',
+    packages=setuptools.find_packages(),
+    setup_requires=['nltk', 'joblib','asgiref','click','regex','sqlparse'],
+)
